5aeeea532b6eb79c2e15eb48e863730e5ef3b41c
Mallikarjun Gajji <mgajji@tekion.com>
2022-05-17 12:52:52 +0000

    Merged in CDMS-45284-fsAvarageReport-api2 (pull request #169)
    CDMS-45284 fsAvearageReport backward compatibility API
    
    * CDMS-45284 fsAvearageReport backward compatibility API
    
    
    Approved-by: Nikita Agrawal

--- service/src/main/java/com/tekion/accounting/fs/api/AnalyticsApi.java
+++ service/src/main/java/com/tekion/accounting/fs/api/AnalyticsApi.java
@@ -1,10 +1,10 @@
 package com.tekion.accounting.fs.api;
 
+import com.tekion.accounting.commons.utils.UserContextUtils;
 import com.tekion.accounting.fs.enums.OEM;
 import com.tekion.accounting.fs.service.analytics.AnalyticsService;
 import com.tekion.accounting.fs.service.compute.models.FSReportDto;
 import com.tekion.core.service.api.TResponseEntityBuilder;
-import com.tekion.core.validation.TValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +21,17 @@ public class AnalyticsApi {
 
     @Autowired
     AnalyticsService analyticsService;
-    @Autowired
-    private TValidator tValidator;
+
+    // for backward compatibility
+    @PostMapping("/fsAverageReport/{oemId}")
+    public ResponseEntity getFsCellSnapshot(@PathVariable OEM oemId, @RequestBody @NotBlank FSReportDto fsReportDto) {
+        log.info("api call received for FSReportingAverage {}", oemId);
+        return TResponseEntityBuilder.okResponseEntity(analyticsService.getFSCellCodeAverage(fsReportDto.getFromTimestamp(),
+                fsReportDto.getToTimestamp(), fsReportDto.getCodes(), oemId.name(), UserContextUtils.getSiteIdFromUserContext()));
+    }
 
     @PostMapping("/fsAverageReport/{oemId}/siteId/{siteId}")
-    public ResponseEntity getFsCellSnapshot(@PathVariable OEM oemId, @PathVariable  String siteId,
+    public ResponseEntity getFsCellSnapshotBySite(@PathVariable OEM oemId, @PathVariable  String siteId,
                                             @RequestBody @NotBlank FSReportDto fsReportDto) {
         log.info("api call received for FSReportingAverage {} {}", oemId, siteId);
         return TResponseEntityBuilder.okResponseEntity(analyticsService.getFSCellCodeAverage(fsReportDto.getFromTimestamp(),
